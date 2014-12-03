1e9472a7e4c913fbe2caa82d0aff304a26c7f81f
ianzapolsky <ianzapolsky@gmail.com>
2014-12-03 16:52:57 -0500

    working on mp3 encoding

--- player.js
+++ player.js
@@ -9,7 +9,8 @@ if (process.argv.length != 3) {
 } 
 
 // this script will produce a single sox command, which will comprise the whole piece
-var command  = 'play';
+//var command  = 'play';
+var command = 'sox --combine mix-power -v 1.0';
 var filename = process.argv[2];
 var contents = fs.readFileSync(filename, 'utf8').split('\n');
 
@@ -17,15 +18,24 @@ for (var i = 0; i < contents.length; i++) {
   if (contents[i] === '') {
     break;
   }
-  var duration   = .1;
-  var wavetype   = 'sin';
-  var frequency  = parseInt(contents[i]);
-  var subcommand = ' "|sox -n -p synth '+duration+' '+wavetype+' '+frequency+'"';
+  var duration  = 1;
+  var wavetype  = 'sin';
+  var frequency = parseInt(contents[i]);
+  var padding   = Math.round((i * .1)*10)/10+'@0.00';
+  var subcommand = ' "|sox -n -p synth '+duration+' '+wavetype+' '+frequency+' pad '+padding+'"';
   command += subcommand;
 }
 
+command += ' ./output.mp3 norm';
+
 fs.writeFile('command.txt', command, function(err) {
   if (err) console.log(err);
+});
+
+exec(command, function(error, stdout, stderr) {
+  console.log(stdout);
+  console.log(stderr);
+  console.log(error);
 });
 
 
