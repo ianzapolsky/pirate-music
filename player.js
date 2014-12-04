aebc103618c98694767ad205543c1be153b9e418
ianzapolsky <ianzapolsky@gmail.com>
2014-12-03 22:47:50 -0500

    chaining mp3 encodes

--- player.js
+++ player.js
@@ -8,9 +8,9 @@ if (process.argv.length != 3) {
   process.exit();
 } 
 
-// this script will produce a single sox command, which will comprise the whole piece
-//var command  = 'play';
-var command = 'sox --combine mix-power -v 1.0';
+var subcommands  = [];
+var mediaryFiles = [];
+
 var filename = process.argv[2];
 var contents = fs.readFileSync(filename, 'utf8').split('\n');
 
@@ -18,24 +18,40 @@ for (var i = 0; i < contents.length; i++) {
   if (contents[i] === '') {
     break;
   }
-  var duration  = 1;
-  var wavetype  = 'sin';
-  var frequency = parseInt(contents[i]);
-  var padding   = Math.round((i * .1)*10)/10+'@0.00';
-  var subcommand = ' "|sox -n -p synth '+duration+' '+wavetype+' '+frequency+' pad '+padding+'"';
-  command += subcommand;
+  var duration   = .1;
+  var wavetype   = 'sin';
+  var frequency  = parseInt(contents[i]);
+  var subcommand = ' "|sox -n -p synth '+duration+' '+wavetype+' '+frequency+'"';
+  subcommands.push(subcommand);
+  if (i % 200 === 0) {
+    var name = 'output'+mediaryFiles.length+'.mp3';
+    var command = 'sox --combine concatenate -v 1.0';
+    for (var j = 0; j < subcommands.length; j++) {
+      command += subcommands[j];
+    }
+    subcommands = [];
+    command += ' '+name+' norm';
+    exec(command, function(error, stdout, stderr) {
+      console.log('wrote '+name);
+    });
+    mediaryFiles.push(name);
+  }
 }
 
-command += ' ./output.mp3 norm';
-
-fs.writeFile('command.txt', command, function(err) {
-  if (err) console.log(err);
-});
+var command = 'sox --combine concatenate -v 1.0';
+for (var i = 0; i < mediaryFiles.length; i++) {
+  command += ' '+mediaryFiles[i];
+}
+command += ' ./final.mp3 norm';
 
 exec(command, function(error, stdout, stderr) {
-  console.log(stdout);
-  console.log(stderr);
-  console.log(error);
+  console.log('wrote final');
 });
+
+//exec(command, function(error, stdout, stderr) {
+//  console.log(stdout);
+//  console.log(stderr);
+//  console.log(error);
+//});
 
 
